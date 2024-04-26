2fc930f8a033fecd0fa61885c5d5f11cb3c98b40
Djenifer Corrêa <142456094+djeniferdc@users.noreply.github.com>
2024-04-26 20:12:17 -0300

    Create detail.dart

---
+++ detail.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:flutter_linkify/flutter_linkify.dart';
+import 'api.dart';
+
+
+class DetailPage extends StatelessWidget {
+  final Movie movie;
+
+  const DetailPage(this.movie, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(movie.name),
+      ),
+      body: Container(
+        padding: const EdgeInsets.all(10.0),
+        child: Column(
+          children: [
+            Center(
+              child: Image(
+                image: NetworkImage(
+                  movie.image,
+                ),
+              ),
+            ),
+            const Text("Link:"),
+            
+            Linkify(
+              onOpen: (link) async {
+                Uri link = Uri.parse(movie.link);
+                if (await canLaunchUrl(link)) {
+                  await launchUrl(link);
+                } else {
+                  throw 'Não foi possível abrir {$movie.link}';
+                }
+              },
+              text: movie.link,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
